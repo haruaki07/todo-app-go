869d3502f528531a34705580c500cc6843fa0119
haruaki07 <misbahulk2236@gmail.com>
2021-12-17 17:12:50 +0700

    feat(mapstore): create todo

--- backend/datastore/mapstore.go
+++ backend/datastore/mapstore.go
@@ -1,13 +1,35 @@
 package datastore
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/Xanvial/todo-app-go/model"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+)
+
 type MapStore struct {
-	data map[string]bool
+	data map[string]model.TodoData
 }
 
 func NewMapStore() *MapStore {
-	newData := make(map[string]bool, 0)
+	newData := make(map[string]model.TodoData, 0)
 
 	return &MapStore{
 		data: newData,
+	}
+}
+
+func (ms *MapStore) CreateTodo(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+
+	id, err := gonanoid.New()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("ArrayStore | title:", title)
+	ms.data[id] = model.TodoData{
+		Title: title,
 	}
 }
